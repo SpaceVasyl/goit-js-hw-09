0761e8142cb4317dcd5e4d1ce6f25bc123d70d4d
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-23 03:35:32 +0200

    start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+startButton.addEventListener("click", handleClick);
+
+function handleClick(event) {
+    event.preventDefault();
+    console.log("press start");
+}
+
+stopButton.addEventListener("click", clickButton);
+
+function clickButton(event) {
+    event.preventDefault();
+    console.log("press stop");
+}
