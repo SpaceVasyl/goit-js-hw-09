61fec02292f581576b57e99d414c5e97db237da8
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-23 23:28:03 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,16 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+const delayInput = document.querySelector(['delay']);
+delayInput.addEventListener("input", (event) => {
+ console.log(event.currentTarget.value);
+});
+const promiseButton = document.querySelector('button');
+promiseButton.addEventListener("click", () => {
+  
+  // function createPromise(position, delay) {
+  //   const shouldResolve = Math.random() > 0.3;
+  //   if (shouldResolve) {
+  //     // Fulfill
+  //   } else {
+  //     // Reject
+  //   }
+  // }
+});
