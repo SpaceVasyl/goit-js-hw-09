675773d0a58dc84addb6d892eccef497ba79ce95
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2022-12-23 01:36:03 +0000

    Deploying to gh-pages from @ SpaceVasyl/goit-js-hw-09@0761e8142cb4317dcd5e4d1ce6f25bc123d70d4d 🚀

---
+++ 01-color-switcher.3e7f1b75.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(e){e.preventDefault(),console.log("press start")})),t.addEventListener("click",(function(e){e.preventDefault(),console.log("press stop")}))}();
+//# sourceMappingURL=01-color-switcher.3e7f1b75.js.map
---
+++ 01-color-switcher.92f14b1f.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",(function(e){e.preventDefault(),console.log("press start")})),t.addEventListener("click",(function(e){e.preventDefault(),console.log("press stop")}));
+//# sourceMappingURL=01-color-switcher.92f14b1f.js.map
