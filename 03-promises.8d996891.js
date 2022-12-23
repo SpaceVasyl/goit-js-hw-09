506c3743427b66690cb76752ff8668067b8f7b53
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2022-12-23 21:28:30 +0000

    Deploying to gh-pages from @ SpaceVasyl/goit-js-hw-09@61fec02292f581576b57e99d414c5e97db237da8 🚀

---
+++ 03-promises.8d996891.js
@@ -0,0 +1,2 @@
+document.querySelector(["delay"]).addEventListener("input",(e=>{console.log(e.currentTarget.value)}));document.querySelector("button").addEventListener("click",(()=>{}));
+//# sourceMappingURL=03-promises.8d996891.js.map
