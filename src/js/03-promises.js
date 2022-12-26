4534863739e692fbb54596850b6ee5d9a9680adc
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-27 00:07:09 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,18 +1,32 @@
 const delayInput = document.querySelector('.form');
-
-const obj = {};
-delayInput.addEventListener("input", (event) => {
-  obj[event.target.name] = event.target.value;
-
-});
 const promiseButton = document.querySelector('button');
-promiseButton.addEventListener("click", createPromise(obj.position, obj.delay));
+const obj = {};
+delayInput.addEventListener("input", inputObj);
+function inputObj(event) {
+  obj[event.target.name] = event.target.value;
+};
+promiseButton.addEventListener("click", formHandler);
+
+function formHandler(event) {
+  event.preventDefault();
+  const { amount, delay, step } = obj;
+  createPromise()
+}
+
 function createPromise(position, delay) {
-  console.log(obj);
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      console.log(obj);
-    } else {
-      console.log(obj);
-    }
-  }
+  const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        promise.then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+      } else {
+        promise.catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      
+      }
+    }, obj.position)
+  })
+}
