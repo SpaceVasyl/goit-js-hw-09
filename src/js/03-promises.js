39b650f4fca5efb9411d7bd508d2ad97f9662576
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-27 00:23:55 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,23 +10,6 @@ promiseButton.addEventListener("click", formHandler);
 function formHandler(event) {
   event.preventDefault();
   const { amount, delay, step } = obj;
-  createPromise()
+  
 }
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        promise.then(({ position, delay }) => {
-          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        })
-      } else {
-        promise.catch(({ position, delay }) => {
-          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-        });
-      
-      }
-    }, obj.position)
-  })
-}
+console.log(obj.delay);
