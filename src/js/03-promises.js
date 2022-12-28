94f560b0d06f175a72dec3221b94ae32e27d8388
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-28 02:07:17 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,7 @@
 const delayInput = document.querySelector('.form');
 const promiseButton = document.querySelector('button');
 const obj = {};
+
 delayInput.addEventListener("input", inputObj);
 function inputObj(event) {
   obj[event.target.name] = event.target.value;
@@ -10,25 +11,30 @@ promiseButton.addEventListener("click", formHandler);
 function formHandler(event) {
   event.preventDefault();
   const { amount, delay, step } = obj;
-  createPromise(obj.amount, obj.delay)
+  for (let i = 1; i <= obj.amount; i++) {
+    if (i <= obj.amount) {
+        createPromise(obj.amount, obj.delay, obj.step).then(({ position, delay, step }) => {
+          console.log(`✅ Fulfilled promise ${i} in ${delay}ms`);
+          console.log(step);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${i} in ${delay}ms`);
+        });
+    }
+  }
+}
+function createPromise(position, delay, step) {
+    return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay, step});
+      }
+        reject({position, delay, step});
+      }, Number(obj.step));
+  })
 }
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-  if (shouldResolve) {
-    createPromise.then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  } else {
-   createPromise.catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-  }
-    }, obj.delay);
-});
-}
 
 
   
