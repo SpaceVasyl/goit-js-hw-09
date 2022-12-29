a58fd857e6bb337042ab457301182022bcb5b987
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-29 04:16:30 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,19 +4,21 @@ import "flatpickr/dist/flatpickr.min.css";
 
 const inputTime = document.querySelector("#datetime-picker");
 const button = document.querySelector("button");
-flatpickr(inputTime);
+button.disabled = true;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr(inputTime, options);
 
 inputTime.addEventListener("input", timeIsMoney);
 function timeIsMoney(event) {
+    button.disabled = false;
     console.dir();
 }
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
