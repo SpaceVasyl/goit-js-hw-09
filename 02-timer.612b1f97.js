e62743233f21b812d04661708fb5ce6bdf40537e
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2022-12-29 01:45:57 +0000

    Deploying to gh-pages from @ SpaceVasyl/goit-js-hw-09@408fa36ba662b11094215a3b488656a8928f0c0c 🚀

--- 02-timer.612b1f97.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("#datetime-picker"),document.querySelector("button");const e=Date.now();setTimeout((()=>{const o=Date.now();console.log("date1",e),console.log("date2",o),console.log(o-e)}),3e3);
-//# sourceMappingURL=02-timer.612b1f97.js.map
--- 02-timer.af05fdd2.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("#datetime-picker"),document.querySelector("button");var e=Date.now();setTimeout((function(){var o=Date.now();console.log("date1",e),console.log("date2",o),console.log(o-e)}),3e3)}();
-//# sourceMappingURL=02-timer.af05fdd2.js.map
