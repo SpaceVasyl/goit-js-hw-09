bb364298bf8573716461b1792c9429dfb835ec5f
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-01-18 19:54:43 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,15 +4,13 @@ const startButton = document.querySelector("[data-start]");
 const stopButton = document.querySelector("[data-stop]");
 let timerNumb = null;
 stopButton.disabled = true;
-const timerActive = false;
 startButton.addEventListener("click", () => {
     elementBody.style.backgroundColor = getRandomHexColor();
     startButton.disabled = true;
     stopButton.disabled = false;
-    timerActive = true;
+    const timerActive = true;
     timerNumb = setInterval(() => {
         elementBody.style.backgroundColor = getRandomHexColor();
-        console.log("assad");
         }, 1000);
 });
 
